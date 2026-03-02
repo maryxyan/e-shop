7c4a9425122d6af0d5a8a6f8332ab3339de965dc
Mute <maryxyan@gmail.com>
2026-03-02 10:35:45 +0200

    Styling
    Added new logo change the top-header active to show bigger underline

--- project/resources/views/layouts/front/app.blade.php
+++ project/resources/views/layouts/front/app.blade.php
@@ -27,22 +27,22 @@
     <script src="{{ asset('https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js')}}"></script>
     <script src="{{ asset('https://oss.maxcdn.com/respond/1.4.2/respond.min.js')}}"></script>
     <![endif]-->
-    <link rel="apple-touch-icon" sizes="57x57" href="{{ asset('favicons/apple-icon-57x57.png')}}">
-    <link rel="apple-touch-icon" sizes="60x60" href="{{ asset('favicons/apple-icon-60x60.png')}}">
-    <link rel="apple-touch-icon" sizes="72x72" href="{{ asset('favicons/apple-icon-72x72.png')}}">
-    <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('favicons/apple-icon-76x76.png')}}">
-    <link rel="apple-touch-icon" sizes="114x114" href="{{ asset('favicons/apple-icon-114x114.png')}}">
-    <link rel="apple-touch-icon" sizes="120x120" href="{{ asset('favicons/apple-icon-120x120.png')}}">
-    <link rel="apple-touch-icon" sizes="144x144" href="{{ asset('favicons/apple-icon-144x144.png')}}">
-    <link rel="apple-touch-icon" sizes="152x152" href="{{ asset('favicons/apple-icon-152x152.png')}}">
-    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('favicons/apple-icon-180x180.png')}}">
-    <link rel="icon" type="image/png" sizes="192x192"  href="{{ asset('favicons/android-icon-192x192.png')}}">
-    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicons/favicon-32x32.png')}}">
-    <link rel="icon" type="image/png" sizes="96x96" href="{{ asset('favicons/favicon-96x96.png')}}">
-    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicons/favicon-16x16.png')}}">
+    <link rel="apple-touch-icon" sizes="57x57" href="{{ asset('assets/images/LOGO.png')}}">
+    <link rel="apple-touch-icon" sizes="60x60" href="{{ asset('assets/images/LOGO.png')}}">
+    <link rel="apple-touch-icon" sizes="72x72" href="{{ asset('assets/images/LOGO.png')}}">
+    <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('assets/images/LOGO.png')}}">
+    <link rel="apple-touch-icon" sizes="114x114" href="{{ asset('assets/images/LOGO.png')}}">
+    <link rel="apple-touch-icon" sizes="120x120" href="{{ asset('assets/images/LOGO.png')}}">
+    <link rel="apple-touch-icon" sizes="144x144" href="{{ asset('assets/images/LOGO.png')}}">
+    <link rel="apple-touch-icon" sizes="152x152" href="{{ asset('assets/images/LOGO.png')}}">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('assets/images/LOGO.png')}}">
+    <link rel="icon" type="image/png" sizes="192x192"  href="{{ asset('assets/images/LOGO.png')}}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('assets/images/LOGO.png')}}">
+    <link rel="icon" type="image/png" sizes="96x96" href="{{ asset('assets/images/LOGO.png')}}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('assets/images/LOGO.png')}}">
     <link rel="manifest" href="{{ asset('favicons/manifest.json')}}">
     <meta name="msapplication-TileColor" content="#ffffff">
-    <meta name="msapplication-TileImage" content="{{ asset('favicons/ms-icon-144x144.png')}}">
+    <meta name="msapplication-TileImage" content="{{ asset('assets/images/LOGO.png')}}">
     <meta name="theme-color" content="#ffffff">
     @yield('css')
     <meta property="og:url" content="{{ request()->url() }}"/>
